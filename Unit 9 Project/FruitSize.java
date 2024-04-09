9a92ed1c4b567ab6890a44d22a87970a4db6d46e
OTrevinoSJ <156972373+OTrevinoSJ@users.noreply.github.com>
2024-04-08 22:06:52 -0500

    Update FruitSize.java

--- Unit 9 Project/FruitSize.java
+++ Unit 9 Project/FruitSize.java
@@ -1,18 +1,19 @@
 public enum FruitSize {
+    // enum constants calling enum constructor
     SMALL (1),
     MEDIUM (2),
     LARGE (3);
 
+    // instance field
     private int size;
 
+    // Enun construtor
     FruitSize(int size) {
         this.size = size;
     }
 
+    // Gets the actual number of sizes SMALL, MEDIUM, LARGE
     public int getNumberSize() {
         return size;
     }
-
-    
-
 }
