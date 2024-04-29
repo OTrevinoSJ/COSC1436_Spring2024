b205a8fb2e28bd8973b4a0f545a749849a83db1a
OTrevinoSJ <156972373+OTrevinoSJ@users.noreply.github.com>
2024-04-28 23:11:42 -0500

    Update CalculableArea.java

--- ProjectShapes/Shapes/CalculableArea.java
+++ ProjectShapes/Shapes/CalculableArea.java
@@ -2,5 +2,8 @@ package Shapes;
 
 public interface CalculableArea {
 
+    /**
+     * This method without an implementation will force any child of the parent class that implements this interface to provide an implementation
+     */
     double calculateArea();
 }
