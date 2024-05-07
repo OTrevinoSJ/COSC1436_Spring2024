4c4358fd13a6e8e25d68b998b42bfcc487cae7d0
OTrevinoSJ <156972373+OTrevinoSJ@users.noreply.github.com>
2024-05-06 23:04:51 -0500

    Add files via upload

--- Unit 11 Project/Shapes/Ellipse.java
+++ Unit 11 Project/Shapes/Ellipse.java
@@ -7,11 +7,24 @@ public class Ellipse extends Shape {
     private double axisOne;
     private double axisTwo;
 
+    /**
+     * This constructs the ellipse shape instance by supplying the constructor with two inputs
+     * @param axes1
+     * @param axis2
+     * @throws InputMismatchException
+     * @throws InvalidShapeArgument
+     */
     public Ellipse(double axes1, double axis2) throws InputMismatchException, InvalidShapeArgument{
         setAxes(axis2, axis2);
         shapeName = this.getClass().getSimpleName();
     }
 
+    /**
+     * This method sets both axes of the shape and also checking for invalid arguments
+     * @param axis1
+     * @param axis2
+     * @throws InvalidShapeArgument
+     */
     public void setAxes(double axis1, double axis2) throws InvalidShapeArgument {
         if (axis1 <= 0 || axis2 <= 0) throw new InvalidShapeArgument();
         else {
@@ -21,11 +34,19 @@ public class Ellipse extends Shape {
 
     }
 
+    /**
+     * This method calculates the area of the ellipse
+     * @return the area
+     */
     @Override
     public double calculateArea() {
         return 3.14*axisOne*axisTwo;
     }
 
+    /**
+     * This method calculates the perimeter of the ellipse
+     * @return the perimeter
+     */
     @Override
     public double calculatePerimeter() {
         double a = axisOne;
