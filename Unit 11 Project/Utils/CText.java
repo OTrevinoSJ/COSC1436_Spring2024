15977435090a73738cf68a0326331ef868a2f392
OTrevinoSJ <156972373+OTrevinoSJ@users.noreply.github.com>
2024-05-07 04:09:21 -0500

    Update CText.java

--- Unit 11 Project/Utils/CText.java
+++ Unit 11 Project/Utils/CText.java
@@ -2,7 +2,13 @@ package Utils;
 
 public class CText implements ConsoleColorUtil {
 
-
+    /**
+     * This method helps by creating an easy standard of text with the addition of a color
+     * I did this because the System.out.print() line statements started to look ridiculous
+     * @param text
+     * @param color
+     * @return
+     */
     public static String cText(String text, String color){
         return color + text + ANSI_RESET;
     }
